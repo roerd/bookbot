d55faba61cf326132fb7c92fcf0f79f64592d090
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-21 21:46:06 +0200

    take book path as CLI arg

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ def get_book_text(path: str | PathLike) -> str:
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
     occurrences = count_character_occurrences(book_text)
