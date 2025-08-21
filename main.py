ff83f96c8da8ffca51e02e350cb41e4b7f7d0000
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-21 21:16:49 +0200

    extract count_words into own file

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from os import PathLike
 
+from stats import count_words
+
 
 def get_book_text(path: str | PathLike) -> str:
     with open(path) as file:
         return file.read()
 
-def count_words(text: str) -> int:
-    words = text.split()
-    return len(words)
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
     print(f"{num_words} words found in the document")
+
 
 if __name__ == "__main__":
     main()
