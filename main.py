d9f533e45ba3c693b66d52e581675dbe47693a26
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-21 21:38:45 +0200

    sort occurrences and print report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from os import PathLike
 
-from stats import count_words, count_character_occurrences
+from stats import count_words, count_character_occurrences, sort_occurrences
 
 
 def get_book_text(path: str | PathLike) -> str:
@@ -9,11 +9,21 @@ def get_book_text(path: str | PathLike) -> str:
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
     num_words = count_words(book_text)
-    print(f"{num_words} words found in the document")
     occurrences = count_character_occurrences(book_text)
-    print(occurrences)
+    sorted_occurrences = sort_occurrences(occurrences)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for d in sorted_occurrences:
+        if d['char'].isalpha():
+            print(f"{d['char']}: {d['num']}")
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -10,3 +10,13 @@ def count_character_occurrences(text: str) -> dict[str, int]:
     text = text.lower()
     counts = Counter(text)
     return counts
+
+
+def get_num(d: dict) -> int:
+    return d["num"]
+
+
+def sort_occurrences(occurrences: dict[str, int]) -> list[dict[str, str | int]]:
+    occurrences_list = [{"char": c, "num": n} for c, n in occurrences.items()]
+    occurrences_list.sort(key=get_num, reverse=True)
+    return occurrences_list
