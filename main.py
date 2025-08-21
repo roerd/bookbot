561558d908252503e89894688c043247e4f166af
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-21 21:22:11 +0200

    implement counting character occurrences

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from os import PathLike
 
-from stats import count_words
+from stats import count_words, count_character_occurrences
 
 
 def get_book_text(path: str | PathLike) -> str:
@@ -12,6 +12,8 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
     print(f"{num_words} words found in the document")
+    occurrences = count_character_occurrences(book_text)
+    print(occurrences)
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
+from collections import Counter
+
+
 def count_words(text: str) -> int:
     words = text.split()
     return len(words)
+
+
+def count_character_occurrences(text: str) -> dict[str, int]:
+    text = text.lower()
+    counts = Counter(text)
+    return counts
